f42f9a8c6ebb84dee73e3cbe9e0b1b88b7111a74
aymiwash <emi.luong@gmail.com>
2023-07-27 17:28:23 +0200

    done lab

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,77 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  async getFullList() {
+    try {      
+      const response = await axios.get(`${this.BASE_URL}/characters`)
+      if (response.status === 200) {
+        return response.data
+      }
+    } catch (error) {
+      console.error(error)
+    }
   }
 
-  getOneRegister () {
-
+  async getOneRegister(id) {
+    try {   
+      const response = await axios.get(`${this.BASE_URL}/characters/${id}`)
+      if (response.status === 200) {
+        return response.data
+      }
+    } catch (error) {
+      console.error(error)
+    }
   }
 
-  createOneRegister () {
-
+  async createOneRegister(newCharacterObj) {
+    try {      
+      const response = await axios.post(`${this.BASE_URL}/characters`, newCharacterObj)
+      if (response.status === 200) {
+        return response.data
+      }
+    } catch (error) {
+      console.error(error)
+      return newCharacterObj
+    }
   }
 
-  updateOneRegister () {
-
+  async updateOneRegister(id, charToUpdate) {
+    try {     
+      const response = await axios.patch(`${this.BASE_URL}/characters/${id}`, charToUpdate)
+      if (response.status === 200) {
+        return response.data
+      }
+    } catch (error) {
+      console.error(error)
+      return "Character not found"
+    }
   }
 
-  deleteOneRegister () {
+  // async updateAllOneRegister(id, charToUpdate) {
+  //   try {     
+  //     const response = await axios.put(`${this.BASE_URL}/characters/${id}`, charToUpdate)
+  //     if (response.status === 200) {
+  //       return response.data
+  //     }
+  //   } catch (error) {
+  //     console.error(error)
+  //     return "Character not found"
+  //   }
+  // }
 
+  async deleteOneRegister(id) {
+    try {      
+      const response = await axios.delete(`${this.BASE_URL}/characters/${id}`)
+      if (response.status === 200) {
+        // return "Character has been successfully deleted"; 
+        return true
+      }
+    } catch (error) {    
+      console.error(error) 
+      // return "Cannot find the character to delete"
+      return false
+    }
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,143 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
 
+  //Selecting the div where characters will be displayed
+  const containerDiv = document.querySelector(".characters-container")
+
+  document.getElementById('fetch-all').addEventListener('click', async function (event) {
+    //Clear the container first
+    containerDiv.innerHTML = ""
+
+    //Get all characters with API
+    const allCharacters = await charactersAPI.getFullList()
+
+    //Then display all characters in container
+    allCharacters.forEach(character => {
+      const newDiv = document.createElement("div")
+      newDiv.className = "character-info"
+      containerDiv.appendChild(newDiv)
+      newDiv.innerHTML = `
+      <div class="id">Id: ${character.id}</div>
+      <div class="name">Name: ${character.name}</div>
+      <div class="occupation">Occupation: ${character.occupation}</div>
+      <div class="cartoon">Is a Cartoon?: ${character.cartoon}</div>
+      <div class="weapon">Weapon: ${character.weapon}</div>`
+    });
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
+    //Clear container
+    containerDiv.innerHTML = ""
 
+    //Focusing on the input value to get the ID
+    const characterId = document.querySelector('#search-input').value;
+
+    //Get one character with API
+    const oneCharacter = await charactersAPI.getOneRegister(characterId)
+
+    //If the char is found
+    if (oneCharacter) {
+      //Create the new div
+      const newDiv = document.createElement("div")
+      newDiv.className = "character-info"
+      containerDiv.appendChild(newDiv)
+      newDiv.innerHTML = `
+      <div class="id">Id: ${oneCharacter.id}</div>
+      <div class="name">Name: ${oneCharacter.name}</div>
+      <div class="occupation">Occupation: ${oneCharacter.occupation}</div>
+      <div class="cartoon">Is a Cartoon?: ${oneCharacter.cartoon}</div>
+      <div class="weapon">Weapon: ${oneCharacter.weapon}</div>`
+    } else {
+      containerDiv.innerHTML = "Character not found"
+    }
+    
+    //Clear input
+    document.querySelector('#search-input').value = ""
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document.getElementById('delete-one').addEventListener('click', async function (event) {
+    //Focusing on the input value to get the ID
+    const characterId = document.querySelector('#delete-input').value;
 
+    //If an ID is entered in input
+    if (characterId.length !== 0) {
+      const deletingChar = await charactersAPI.deleteOneRegister(characterId)
+      //If deletion ok
+      if (deletingChar) {
+        containerDiv.innerHTML = "Successfully deleted"
+      } else {
+        containerDiv.innerHTML = "Character not found"
+      }
+    }
   });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  document.getElementById('edit-character-form').addEventListener('submit', async function (event) {
+    //Getting all inputs value
+    const editId = document.querySelector("#edit-id").value
+    const editName = document.querySelector("#edit-name").value
+    const editOccupation = document.querySelector("#edit-occupation").value
+    const editWeapon = document.querySelector("#edit-weapon").value
+    const editCartoon = document.querySelector("#edit-cartoon").checked
 
+    //Store all values in a object
+    const editChar = {
+      name: editName,
+      occupation: editOccupation,
+      weapon: editWeapon,
+      cartoon: editCartoon
+    }
+
+    //Editing char on API
+    const editingChar = await charactersAPI.updateOneRegister(editId, editChar)
+    if (editingChar) {
+      containerDiv.innerHTML = "Successfully updated"
+    } else {
+      containerDiv.innerHTML = "Character not found"
+    }
   });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  document.getElementById('new-character-form').addEventListener('submit', async function (event) {
+    //Getting all inputs value
+    const createName = document.querySelector("#create-name").value
+    const createOccupation = document.querySelector("#create-occupation").value
+    const createWeapon = document.querySelector("#create-weapon").value
+    const createCartoon = document.querySelector("#create-cartoon").checked
 
+    //Store all values in a object
+    const createChar = {
+      name: createName,
+      occupation: createOccupation,
+      weapon: createWeapon,
+      cartoon: createCartoon
+    }
+
+    //Creating new char in API
+    const createNewChar = await charactersAPI.createOneRegister(createChar)
+    if (createNewChar) {
+      console.log("Successfully created", createNewChar);
+    }
   });
 });
+
+
+      // //Create the name div
+      // const newNameDiv = document.createElement("div")
+      // newNameDiv.className = "name"
+      // newNameDiv.innerHTML = `Character: ${character.name}`
+      // newDiv.appendChild(newNameDiv)
+      // //Create the occupation div
+      // const newOccupationDiv = document.createElement("div")
+      // newOccupationDiv.className = "occupation"
+      // newOccupationDiv.innerHTML = `Character Occupation: ${character.occupation}`
+      // newDiv.appendChild(newOccupationDiv)
+      // //create the cartoon div
+      // const newCartoonDiv = document.createElement("div")
+      // newCartoonDiv.className = "cartoon"
+      // newCartoonDiv.innerHTML = `Is a Cartoon ?: ${character.cartoon}`
+      // newDiv.appendChild(newCartoonDiv)
+      // //Create the weapon div
+      // const newWeaponDiv = document.createElement("div")
+      // newWeaponDiv.className = "weapon"
+      // newWeaponDiv.innerHTML = `Character Weapon: ${character.weapon}`
+      // newDiv.appendChild(newWeaponDiv)
